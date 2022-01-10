ead04f65853ecad39009858cf1e5d765a26f3ef3
steefanyalvees <stefanyanjoos@hotmail.com>
2022-01-10 13:42:59 +0000

    js

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ const express = require("express"), // listing the modulos to allow the http req
             server = http.createServer(router);
 
             router.use(express.static(path.resolve(__dirname,'views'))); //serving the static content from views folder
+            
+            
+            //listing to the income connection
+            server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+                const addr = server.address();
+                console.log('Server listening at', addr.address + ':' + addr.port)});
+            
