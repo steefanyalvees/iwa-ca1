18f3a896ee73b802cd391902ff4b93fc3008ba76
steefanyalvees <stefanyanjoos@hotmail.com>
2022-01-10 14:19:32 +0000

    index.js

--- index.js
+++ index.js
@@ -34,5 +34,6 @@ const express = require("express"), // listing the modulos to allow the http req
             //listing to the income connection
             server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
                 const addr = server.address();
-                console.log('Server listening at', addr.address + ':' + addr.port)});
+                console.log('Server listening at', addr.address + ':' + addr.port)
+            });
             
